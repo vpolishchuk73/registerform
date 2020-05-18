4cf4b43d906ccbfc97a812e693555f8d2ab9fb16
vladimir polischuk <vnpolisch@yahoo.com>
2020-05-18 12:12:29 +0300

    second commit

--- script2.js
+++ script2.js
@@ -39,8 +39,38 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase + input.id.slice(1);
 }
 
+function checkLength(input,min,max) {
+    if(input.value.length < min) {
+        showError(input, 'must be at least 3 characters');
+    } else if (input.value.length > max) {
+        showError(input, 'must be less than 15 characters');
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+//Check password match
+function checkPasswordsMatch(input1,input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Password do not match');
+    }
+}
+
 //Event listener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     checkRequired([username, email, password, password2]);
+    checkLength(username,3,15);
+    checkLength(password,6,25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
